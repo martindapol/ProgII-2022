7df6cf32065d74505e627e0e70cb49ad16725211
unknown <martindapol@gmail.com>
2022-10-19 22:32:39 -0300

    Web API + .dll

---
+++ Semana 10/CarpinteriaSLN/DataAPI/fachada/DataApiImp.cs
@@ -0,0 +1,31 @@
+﻿using CarpinteriaApp.datos.Implementacion;
+using CarpinteriaApp.datos.Interfaz;
+using CarpinteriaApp.dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAPI.fachada
+{
+    public class DataApiImp : IDataApi
+    {
+        private IDaoPresupuesto dao;
+
+        public DataApiImp()
+        {
+            dao = new PresupuestoDao();
+        }
+
+        public List<Producto> GetProductos()
+        {
+            return dao.ObtenerProductos();
+        }
+
+        public bool SavePresupuesto(Presupuesto presupuesto)
+        {
+            return dao.Crear(presupuesto);
+        }
+    }
+}
